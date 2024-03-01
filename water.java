7d08c36f2437350d66b107b70d9e2b5b41d8318b
YUVASRI06 <yuvasriperumalsamy0609@gmail.com>
2024-03-01 20:07:29 +0530

    erwre

--- water.java
+++ water.java
@@ -3,6 +3,7 @@ public class HelloWorld {
         System.out.println("Hello, World!");
           System.out.println("This is yuva");
           System.out.println("Hello, " + name + "! Welcome to the Simple Greeting Program.");
+          System.out.println("Hello, " + name + "! Welcome to the Simple Greeting Program.");
     }
 
 
